c95caa33fa80cd16a105dbe0026ab031c62eebad
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-09-19 16:02:03 +0200

    js array

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ rangeInput.addEventListener(
   "input",
   (e) => (output.textContent = e.target.value)
 );
+
+// Випадаючий текст з рядка = переробити на JS
+$(document).ready(function () {
+  $(".block__box-title").click(function (event) {
+    if ($(".block__box-wrap").hasClass("one")) {
+      $("block__box-title").not($(this)).removeClass("active");
+      $("block__box-text").not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClasss("active").next().slideToggle(300);
+  });
+});
